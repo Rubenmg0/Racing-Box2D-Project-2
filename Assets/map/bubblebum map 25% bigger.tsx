97047150636a5ed925a2b7b8d6c3af5023b014b6
Ruzz <ruben.mateo.guerrero@estudiantat.upc.edu>
2025-12-14 14:15:54 +0100

    Upgrade Map + Collision Layer
    Implementation of barriers and grass on the map
    Creation of collision layer

--- Assets/map/bubblebum map 25% bigger.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="bubblebum map 25% bigger" tilewidth="160" tileheight="160" tilecount="4" columns="2">
- <image source="CuteRoad/bubblebum map 25% bigger.aseprite" width="460" height="460"/>
-</tileset>
