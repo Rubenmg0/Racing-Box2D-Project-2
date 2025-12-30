c788529cbe147bf78e2e38538be9e25b61f14f38
irentren <icalatravasierra@gmail.com>
2025-12-30 19:58:37 +0100

    newMusic

---
+++ Assets/Cars/wheel_animation_spritesheet.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="wheel_animation_spritesheet" tilewidth="4" tileheight="8" tilecount="8" columns="8">
+ <image source="../Cars/NormalCars/wheel_animation_spritesheet.png" width="32" height="8"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="95"/>
+   <frame tileid="1" duration="95"/>
+   <frame tileid="2" duration="95"/>
+   <frame tileid="3" duration="95"/>
+   <frame tileid="4" duration="95"/>
+   <frame tileid="5" duration="95"/>
+   <frame tileid="6" duration="95"/>
+   <frame tileid="7" duration="95"/>
+  </animation>
+ </tile>
+</tileset>
